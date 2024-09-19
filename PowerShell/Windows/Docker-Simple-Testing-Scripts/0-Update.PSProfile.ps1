aa01b0804272a3fd779b46e556bad18587e25f64
aollivierre <abdullahollivierre@gmail.com>
2024-09-19 10:06:03 -0700

    updating path to run scipt

--- PowerShell/Windows/Docker-Simple-Testing-Scripts/0-Update.PSProfile.ps1
+++ PowerShell/Windows/Docker-Simple-Testing-Scripts/0-Update.PSProfile.ps1
@@ -6,7 +6,7 @@ function rpd {
     )
     
     # Dot-source the Docker script to make the 'runpsindocker' function available in the current session
-    . "C:\code\docker\PowerShell\Windows\Docker-Simple-Testing-Scripts\4-RunPSwithDocker.ps1"
+    . "C:\code\docker\PowerShell\Windows\Docker-Simple-Testing-Scripts\0.1-RunPSwithDocker.ps1"
 
     # Call the 'runpsindocker' function with the provided script path
     runpsindocker $localScriptPath
